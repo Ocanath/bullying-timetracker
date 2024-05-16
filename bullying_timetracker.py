f1ec3a76ae0a46ff9c3ec6a3ea7967753cdcd74a
ocanath <ocanath@gmail.com>
2024-05-16 12:14:29 -0700

    added optional sleep to reduce resource utilization of this stupid ass script

--- bullying_timetracker.py
+++ bullying_timetracker.py
@@ -7,6 +7,7 @@ if __name__ == "__main__":
 	parser.add_argument('--hourly_rate', type=float, default=0, help='what you get paid in dollars per hour')
 	parser.add_argument('--annual_salary', type=float, default=0, help='what you get paid per year')
 	parser.add_argument('--dollar_increment', type=float, default=0.01, help="Amount in dollars to increment on the display")
+	parser.add_argument('--sleep',type=float, default=0, help="time to sleep per loop iteration. Can interfere with proper display rate update")
 	args = parser.parse_args()
 
 	if(args.hourly_rate == 0 and args.annual_salary == 0):
@@ -18,6 +19,7 @@ if __name__ == "__main__":
 	elif(args.annual_salary != 0):
 		hourly_rate = (args.annual_salary/52/40)
 		print("Using", hourly_rate, "as hourly rate")
+
 	cost_incurred = 0
 	prev_cost = 0
 	start_time = time.time()
@@ -32,7 +34,9 @@ if __name__ == "__main__":
 			if(int( (cost_incurred-prev_cost) / args.dollar_increment ) >= 1):
 				print("$", round(cost_incurred, 2))
 				prev_cost = cost_incurred
-			# time.sleep(1)
+
+			if(args.sleep != 0):
+				time.sleep(args.sleep)
    
 	except KeyboardInterrupt:
 		pass
